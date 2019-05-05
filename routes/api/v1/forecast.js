d94b43131ac6b551d3ab6dc68034f38ed905b135
Scott Thomas <smthom05@gmail.com>
2019-05-05 11:13:10 -0600

    forecast data is being returned. Still need to have some sad path catches.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -10,15 +10,20 @@ router.get('/', function(req, res) {
   var geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + req.query.location + "&key=" + process.env.GEOCODE_KEY;
 
   fetch(geocodeUrl)
-  .then((response) => {return response.json()};)
+  .then(response => {return response.json();})
   .then(result => {
-    var lat = result.results[0].geometry.lat
-    var lng = result.results[0].geometry.lng
-
-    forecastUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY'}/${lat},${lng}?exclude=minutely,alerts,flags`
+    var lat = result.results[0].geometry.location.lat
+    var lng = result.results[0].geometry.location.lng
+    var forecastUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}?exclude=minutely,alerts,flags`;
 
     fetch(forecastUrl)
-    .then(response) => {response.json();}
+    .then((forecastResponse) => {
+      return forecastResponse.json();
+    })
+    .then(forecastResult => {
+      res.status(200).send(forecastResult)
+    })
+    .catch(err => { console.log(err) })
   })
   .catch(err => { console.log(err) })
 })
