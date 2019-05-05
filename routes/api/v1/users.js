e354f53738e5ec21f3b045ee37ab896ac537b4d9
Scott Thomas <smthom05@gmail.com>
2019-05-05 11:27:06 -0600

    removed old commented out api key attempt from account creation route

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-// const crypto = require('crypto')
 const hat = require('hat')
 var router = express.Router();
 var User = require('../../../models').User;
@@ -31,15 +30,5 @@ router.post('/', function(req,res) {
     res.status(401).send("Invalid email")
   }
 });
-
-
-// function generateApiKey() {
-//   crypto.randomBytes(32, (err, buf) => {
-//     if (err) throw err;
-//
-//     console.log(buf.toString('hex'))
-//     return buf.toString('hex');
-//   })
-// };
 
 module.exports = router;
