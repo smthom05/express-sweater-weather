99412281e4f9feba10005f13042a417dee11ff4c
Scott Thomas <smthom05@gmail.com>
2019-05-05 11:56:53 -0600

    added check to make sure an api key is sent with the request or it will throw a 401

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -9,36 +9,41 @@ var User = require('../../../models').User;
 
 // City Forecast
 router.get('/', function(req, res) {
-  User.findOne({
-    where: {
-      apiKey: req.body.api_key
-    }
-  })
-  .then(user => {
-    if (user !== null) {
-      var geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + req.query.location + "&key=" + process.env.GEOCODE_KEY;
+  if (req.body.api_key) {
+    User.findOne({
+      where: {
+        apiKey: req.body.api_key
+      }
+    })
+    .then(user => {
+      if (user !== null) {
+        var geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + req.query.location + "&key=" + process.env.GEOCODE_KEY;
 
-      fetch(geocodeUrl)
-      .then(response => {return response.json();})
-      .then(result => {
-        var lat = result.results[0].geometry.location.lat
-        var lng = result.results[0].geometry.location.lng
-        var forecastUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}?exclude=minutely,alerts,flags`;
+        fetch(geocodeUrl)
+        .then(response => {return response.json();})
+        .then(result => {
+          var lat = result.results[0].geometry.location.lat
+          var lng = result.results[0].geometry.location.lng
+          var forecastUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}?exclude=minutely,alerts,flags`;
 
-        fetch(forecastUrl)
-        .then((forecastResponse) => {
-          return forecastResponse.json();
+          fetch(forecastUrl)
+          .then((forecastResponse) => {
+            return forecastResponse.json();
+          })
+          .then(forecastResult => {
+            res.status(200).send(forecastResult)
+          })
+          .catch(err => { console.log(err) })
         })
-        .then(forecastResult => {
-          res.status(200).send(forecastResult)
-        })
-        .catch(err => { console.log(err) })
-      })
-     } else {
-       res.setHeader("Content-Type", "application/json");
-       res.status(401).send("Invalid Api Key");
-    };
-  });
+       } else {
+         res.setHeader("Content-Type", "application/json");
+         res.status(401).send("Invalid Api Key");
+      };
+    });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send("Invalid Api Key");
+  };
 });
 
 module.exports = router;
